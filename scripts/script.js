672db28c1ee8bd647c877af682cb33a29a068fbd
aaronmccollum <mccollumaarons@gmail.com>
2024-08-03 07:14:05 -0400

    bug fixes for window reload and text generator

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,8 @@ const counterTextarea = document.getElementById('text-counter-textarea');
 const generatorInput = document.getElementById('text-generator-input');
 const counterResults = document.getElementById('counter-results');
 const generatorResults = document.getElementById('generator-results');
-const generatorInputValue = generatorInput.value;
 const counterReset = document.getElementById('counter-reset-button');
 const generatorReset = document.getElementById('generator-reset-button');
-
 const dummyText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 // Function to count chars in string
@@ -36,17 +34,26 @@ function generateChars(num) {
 
 // Generates lorem ipsum text when 'Generate Text' button is clicked
 generatorButton.addEventListener('click', function() {
+    const generatorInputValue = generatorInput.value;
     const generatedResults = generateChars(generatorInputValue);
+    console.log(generateChars(generatorInputValue));
     generatorResults.innerText = generatedResults;
 });
 
-// Resets number in user input area
+// Resets number in counter user input area
 counterReset.addEventListener('click', function() {
     counterTextarea.value = '';
 })
 
 // Resets the results paragraph
 generatorReset.addEventListener('click', function() {
-    generatorInput.value = 0;
+    generatorInput.value = '';
     generatorResults.innerText = '';
 })
+
+// Resets the page when the page is reloaded in the browser
+window.addEventListener('load', function() {
+    generatorInput.value = '';
+    generatorResults.innerText = '';
+    counterTextarea.value = '';
+});
